b930440270df48ddd4b52c922c9fdf4b83b9a65c
Juxhin (Eugene) Bakalli <sofokli1@gmail.com>
2019-12-02 11:34:34 +0200

    💄 (#83)

--- TogglTrack/Utils/DateExtensions.swift
+++ TogglTrack/Utils/DateExtensions.swift
@@ -8,15 +8,25 @@
 
 import Foundation
 
-private var shortTimeFormatter: DateFormatter = {
+private let shortTimeFormatter: DateFormatter = {
     let formatter = DateFormatter()
     formatter.timeStyle = .short
     return formatter
 }()
 
-private var simpleDateFormatter: DateFormatter = {
+private let simpleDateFormatter: DateFormatter = {
     let formatter = DateFormatter()
     formatter.dateFormat = "E, d MMM"
+    return formatter
+}()
+
+private let encoderDateFormatter: DateFormatter =
+{
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+    formatter.calendar = Calendar(identifier: .iso8601)
+    formatter.timeZone = TimeZone(secondsFromGMT: 0)
+    formatter.locale = Locale(identifier: "en_US_POSIX")
     return formatter
 }()
 
@@ -45,5 +55,10 @@ extension Date
     public func toTimeString() -> String
     {
         return shortTimeFormatter.string(from: self)
+    }
+    
+    public func toServerEncodedDateString() -> String
+    {
+        return encoderDateFormatter.string(from: self).replacingOccurrences(of: "+0000", with: "+00:00")
     }
 }
