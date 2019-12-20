7f26e8ba0c2b408f60a21a8789303bbc06a39a59
Juxhin (Eugene) Bakalli <sofokli1@gmail.com>
2019-12-20 09:08:44 +0200

    🚧 Add UIColor helper

--- TogglTrack/Extensions/ColorExtension.swift
+++ TogglTrack/Extensions/ColorExtension.swift
@@ -32,6 +32,28 @@ extension Color
         )
     }
     
+    public func toUIColor() -> UIColor {
+
+        let components = self.components()
+        return UIColor(red: components.r, green: components.g, blue: components.b, alpha: components.a)
+    }
+
+    private func components() -> (r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat) {
+
+        let scanner = Scanner(string: self.description.trimmingCharacters(in: CharacterSet.alphanumerics.inverted))
+        var hexNumber: UInt64 = 0
+        var r: CGFloat = 0.0, g: CGFloat = 0.0, b: CGFloat = 0.0, a: CGFloat = 0.0
+
+        let result = scanner.scanHexInt64(&hexNumber)
+        if result {
+            r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+            g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+            b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+            a = CGFloat(hexNumber & 0x000000ff) / 255
+        }
+        return (r, g, b, a)
+    }
+    
     // App colors
     public static var togglGreen = Color(red: 75/255, green: 200/255, blue: 0)
     public static var togglRed = Color(red: 241/255, green: 18/255, blue: 18/255)
